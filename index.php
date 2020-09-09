3ce04b0b327a611435e438090db3e30c0cdb10d1
einnabno <70646362+einnabno@users.noreply.github.com>
2020-09-09 22:37:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 	</head>
 	<body>
-		<div class="text">ORDER AND CALL US NOW!</div>
+		<div class="text">ORDER AND CALL US NOW! </div>
 		<div class="feature"></div>
 	</body>
 </html>
