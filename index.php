f9701d17c35587136d06fb5034acd6a272b23a2b
einnabno <70646362+einnabno@users.noreply.github.com>
2020-09-09 22:32:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+	<body>
+		<div class="text">ORDER AND CALL US NOW!</div>
+		<div class="feature"></div>
+	</body>
+</html>
